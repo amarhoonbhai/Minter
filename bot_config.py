c42a6438d07f2b25d44bc569ed9515ae6f9b815f
अमर <120897603+amarhoonbhai@users.noreply.github.com>
2025-07-11 11:17:19 +0530

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -1,16 +1,16 @@
 # bot_config.py
 
-BOT_TOKEN = 'YOUR_BOT_TOKEN_HERE'
+BOT_TOKEN = '7766866622:AAEedlDl2M2BWh-LLC5DvXdEURnCXlgHULI'
 BOT_USERNAME = 'GiftsMintersBot'
 TELEGRAM_USER_ID = 7775062794
 CHANNEL_USERNAME = '@GiftsMinters'
 
-API_ID = 123456
-API_HASH = 'your_api_hash_here'
-PHONE_NUMBER = '+1234567890'
+API_ID = 27018379
+API_HASH = '2fcf836dc55474ea1d593db8bf0947ae'
+PHONE_NUMBER = '+917300897306'
 
 STICKER_SET_NAME = f'giftsminters_by_{BOT_USERNAME}'
-STICKER_SET_TITLE = 'NFT Gifts by GiftsMinters'
+STICKER_SET_TITLE = 'NFT Gifts by @GiftsMinters'
 
 GIFT_LIST_URL = 'https://cdn.changes.tg/gifts/id-to-name.json'
 GIFT_INFO_URL = 'https://cdn.changes.tg/gift/{}'
